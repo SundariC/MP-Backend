cc24a09690b9d676a10f385dde7e5f20973c33d5
Sundari <csundari2000@gmail.com>
2025-12-22 21:13:53 +0530

    added

--- Models/Booking.js
+++ Models/Booking.js
@@ -1,36 +1,48 @@
 import mongoose from "mongoose";
 
 const bookingSchema = new mongoose.Schema({
-    client: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: "User", 
-        required: true 
-    },
-    counselor: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: "User", 
-        required: true 
-    },
-    sessionType: { 
-        type: String, 
-        required: true 
-    },
-    appointmentDate: { 
-        type: Date, 
-        required: true 
-    },
-    status: { 
-        type: String, 
-        default: "pending" 
-    },
-    isPaid: { 
-        type: Boolean, 
-        default: false 
-    },
-    videoLink: {
-        type: "String",
-        default: ""
-    }
+  client: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  counselor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+
+  appointmentDate: {
+    type: Date,
+    required: true
+  },
+  timeSlot: {
+    type: String,
+    required: true
+  },
+
+  amount: {
+    type: Number,
+    required: true
+  },
+
+  paymentStatus: {
+    type: String,
+    enum: ["PENDING", "PAID"],
+    default: "PENDING"
+  },
+
+  sessionStatus: {
+    type: String,
+    enum: ["UPCOMING", "COMPLETED", "CANCELLED"],
+    default: "UPCOMING"
+  },
+
+  videoLink: {
+    type: String,
+    default: ""
+  }
+
 }, { timestamps: true });
 
 export default mongoose.model("Booking", bookingSchema);
