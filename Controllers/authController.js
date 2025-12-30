fdba06d2124a32f5eab7ce21c3dbb891ffd3d31f
Sundari <csundari2000@gmail.com>
2025-12-30 14:31:09 +0530

    added

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -37,6 +37,71 @@ export const register = async (req, res) => {
 };
 
 // 2. Login User
+// export const login = async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+
+//     const user = await User.findOne({ email });
+//     if (!user) {
+//       return res.status(401).json({ message: "Invalid credentials" });
+//     }
+
+//     const isPasswordValid = await bcrypt.compare(password, user.password);
+//     if (!isPasswordValid) {
+//       return res.status(401).json({ message: "Invalid credentials" });
+//     }
+
+//     const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+//       expiresIn: "1h",
+//     });
+//     user.token = token;
+//     await user.save();
+
+//     res
+//       .status(200)
+//       .json({ message: "Login successful", token: token, role: user.role });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: "Server error Error in login" });
+//   }
+// };
+// export const login = async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+
+//     // Field validation check
+//     if (!email || !password) {
+//       return res.status(400).json({ message: "Email and password are required" });
+//     }
+
+//     const user = await User.findOne({ email });
+//     if (!user) return res.status(400).json({ message: "Invalid Credentials" });
+
+//     const isMatch = await bcrypt.compare(password, user.password);
+//     if (!isMatch) return res.status(400).json({ message: "Invalid Credentials" });
+
+//     // JWT Generation
+//     const token = jwt.sign(
+//       { id: user._id, role: user.role },
+//       process.env.JWT_SECRET,
+//       { expiresIn: "1d" }
+//     );
+
+//     // Sending back unified data structure
+//     res.status(200).json({
+//       message: "User Logged In Successfully",
+//       token,
+//       user: {
+//         id: user._id,
+//         email: user.email,
+//         fullName: user.fullName,
+//         role: user.role
+//       }
+//     });
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
